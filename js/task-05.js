1badb78eda1a0f91a8c09e7a81a7e82cbd433025
belon-creator <belon8911@gmail.com>
2020-05-02 20:49:33 +0300

    add 5th task

---
+++ js/task-05.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log('=================== task-5 ======================');
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const propArr = [];
+  for (let item of arr) {
+    if (prop in item) {
+      propArr.push(item[prop]);
+    }
+  }
+  return propArr;
+};
+
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
